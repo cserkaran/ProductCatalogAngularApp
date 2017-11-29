528c9a146bf2a9613c5ca4b19c045fb814331478
Karan Sharma <cserkaran@gmail.com>
2017-11-29 23:06:07 +1100

    Covered upto module 11 with Observables.

--- APM/src/app/products/product-list.component.ts
+++ APM/src/app/products/product-list.component.ts
@@ -1,60 +1,55 @@
-import { Component,OnInit } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { IProduct } from "./product";
+import { ProductService } from "./product.service";
 
 @Component({
-    selector:"pm-products",
-    templateUrl:"./product-list.component.html",
-    styleUrls:['./product-list.component.css']
+    selector: "pm-products",
+    templateUrl: "./product-list.component.html",
+    styleUrls: ['./product-list.component.css']
 })
 
 export class ProductListComponent implements OnInit {
-    
- pageTitle : string = 'Product List'; 
- imageWidth : number = 50;
- imageMargin : number = 2;
- showImage : boolean = false;
- listFilter : string='cart';
 
- products : IProduct[] = 
- [
-    {
-        "productId": 1,
-        "productName": "Leaf Rake",
-        "productCode": "GDN-0011",
-        "releaseDate": "March 19, 2016",
-        "description": "Leaf rake with 48-inch wooden handle.",
-        "price": 19.95,
-        "starRating": 3.2,
-        "imageUrl": "http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
-    },
-    {
-        "productId": 2,
-        "productName": "Garden Cart",
-        "productCode": "GDN-0023",
-        "releaseDate": "March 18, 2016",
-        "description": "15 gallon capacity rolling garden cart",
-        "price": 32.99,
-        "starRating": 4.2,
-        "imageUrl": "http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
-    },
-    {
-        "productId": 5,
-        "productName": "Hammer",
-        "productCode": "TBX-0048",
-        "releaseDate": "May 21, 2016",
-        "description": "Curved claw steel hammer",
-        "price": 8.9,
-        "starRating": 4.8,
-        "imageUrl": "http://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png"
+    pageTitle: string = 'Product List';
+    imageWidth: number = 50;
+    imageMargin: number = 2;
+    showImage: boolean = false;
+    _listFilter: string;
+    products: IProduct[] = [];
+    filteredProducts: IProduct[];
+    errorMessage: string;
+
+    constructor(private _productService: ProductService) {
+       // this.listFilter = 'cart';
     }
- ];
 
- toggleImage() : void{
-    this.showImage = !this.showImage;
- }
+    get listFilter(): string {
+        return this._listFilter;
+    }
+    set listFilter(value: string) {
+        this._listFilter = value;
+        this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
+    }
 
- ngOnInit(){
-     console.log('On Init');
- }
+    toggleImage(): void {
+        this.showImage = !this.showImage;
+    }
 
+    performFilter(filterBy: string): IProduct[] {
+        filterBy = filterBy.toLocaleLowerCase();
+        return this.products.filter((product: IProduct) =>
+            product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
+    }
+
+    ngOnInit() {
+        this._productService.getProducts()
+        .subscribe(products => { 
+            this.products = products; 
+            this.filteredProducts = this.products; 
+        }, error => this.errorMessage = <any>error);
+    }
+
+    onRatingClicked(message: string): void {
+        this.pageTitle = 'Product List: ' + message;
+    }
 }
---
+++ APM/src/app/products/product.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from "@angular/core";
+import { IProduct } from "./product";
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/throw';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/map';
+import { HttpResponse } from "@angular/common/http/src/response";
+
+@Injectable()
+export class ProductService{
+   
+    private _productUrl = './api/products/products.json';
+
+    constructor(private _http : HttpClient){ }
+
+    
+    getProducts(): Observable<IProduct[]> {
+        return this._http.get<IProduct[]>(this._productUrl)
+            .do(data => console.log('All: ' + JSON.stringify(data)))
+            .catch(this.handleError);
+    }
+
+    handleError(err: HttpErrorResponse): any {
+        console.log(err.message);
+        return Observable.throw(err.message);
+    }
+
+}
